179be8b18c22b1bff81d291b28500fddb0a19946
Simon Shi <simon@microtrust.com.cn>
2017-01-22 15:04:19 +0800

    fix 1:support zero len string.  fix 2: 9 is returned as 09

--- checksum.py
+++ checksum.py
@@ -2,12 +2,17 @@
 
 def checksum(target):
     sum = 0;
+    if target is None or len(target) == 0:
+        return "00"
     for c in target:
         sum += ord(c);
-    result= "%2x" %( sum % 256);
+    result= "%02x" %( sum % 256);
     return result
 
 
 if __name__ == '__main__':
-    #result shoud be '2a'
+    #result should be '2a'
     print checksum("qSupported:multiprocess+;qRelocInsn+")
+
+    #result should be '09'
+    print checksum("Hc-1")
